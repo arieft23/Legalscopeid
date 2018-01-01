19d2fae623c63bd13414d50a8f471f71632226b0
arieft23 <arieftritomo@gmail.com>
2018-01-01 21:07:32 +0700

    set map to draggable

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
 				<script>
 					function myMap() {
 						var myCenter = new google.maps.LatLng(-6.369058, 106.831989);
-						var mapProp = {center:myCenter, zoom:16, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+						var mapProp = {center:myCenter, zoom:16, scrollwheel:false, draggable:true, mapTypeId:google.maps.MapTypeId.ROADMAP};
 						var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 						var marker = new google.maps.Marker({position:myCenter});
 						marker.setMap(map);
